aa1166d2f2db78d218089fb9d6eafc0850f46d6b
David Vittor <david.vittor@slyp.com.au>
2020-06-12 13:43:37 +1000

    Updated datastore

--- infra/datastore/contact.sql
+++ infra/datastore/contact.sql
@@ -10,7 +10,7 @@ drop type if exists relation;
 drop type if exists groups;
 
 CREATE TYPE relation AS ENUM ('parent', 'child', 'spouse', 'adpotee', 'closefriend');
-CREATE TYPE groups AS ENUM ('family', 'friends', 'work');
+CREATE TYPE gender AS ENUM ('male', 'female');
 
 create table account (
   id serial primary key,
@@ -42,20 +42,29 @@ create table contact (
   death date,
   dates jsonb,
   address_id bigint,
-  gender varchar(6),
+  gender gender,
   notes text,
-  groups groups[],
-  custom_groups jsonb,
+  groups int[],
   relations jsonb,
   links jsonb,
-  tags jsonb,
+  tags int[],
   sort_order int not null default 99,
   created_at timestamp not null default now(),
   updated_at timestamp,
   deleted_at timestamp
 );
 
-create table custom_groups (
+create table groups (
+  id serial primary key,
+  created_by bigint not null REFERENCES account(id),
+  label varchar(64) not null,
+  created_at timestamp not null default now(),
+  updated_at timestamp,
+  deleted_at timestamp,
+  unique (created_by,label)
+);
+
+create table tag (
   id serial primary key,
   created_by bigint not null REFERENCES account(id),
   label varchar(64) not null,
@@ -77,7 +86,6 @@ create table address (
   state char(3) not null default 'NSW',
   country char(2) not null default 'AU',
   sort_order int not null default 99,
-  tags jsonb,
   created_at timestamp not null default now(),
   updated_at timestamp,
   deleted_at timestamp
@@ -87,17 +95,21 @@ alter table account add constraint fk_account_profile foreign key (contact_id) R
 alter table contact add constraint fk_contact_address foreign key (address_id) REFERENCES address(id) ON DELETE CASCADE;
 
 insert into account (username, password) VALUES ('admin',md5('admin'));
+insert into groups (created_by, label) VALUES (1,'family');
+insert into groups (created_by, label) VALUES (1,'friends');
+insert into groups (created_by, label) VALUES (1,'work');
+
 insert into contact (created_by, name) VALUES (1,'first contact');
 update account set contact_id=1 where username='admin';
 
-insert into custom_groups (created_by, label) VALUES (1, 'club');
-insert into contact (created_by, name, groups, custom_groups, relations) VALUES (1,'second contact', '{family,friends}'::groups[], '["club"]','[{"rid":"parent", "cid": "1"}]');
+insert into groups (created_by, label) VALUES (1, 'club');
+insert into contact (created_by, name, groups, relations, gender) VALUES (1,'second contact', '["family", "club"]','[{"rid":"parent", "cid": "1"}]', 'female');
 
 insert into address (created_by, contact_id, street, postcode) VALUES (1,1,'street2',2000);
 insert into address (created_by, contact_id, street, postcode) VALUES (1,1,'street3',3000);
 insert into address (created_by, contact_id, street, postcode) VALUES (1,2,'street4',4000);
 insert into address (created_by, contact_id, street, postcode) VALUES (1,2,'street5',5000);
 
-select c.id, c.name, c.groups, c.custom_groups, c.relations from contact c, account a where c.created_by=a.id and a.id=1 order by sort_order,id;
+select c.id, c.name, c.gender, c.groups, c.relations from contact c, account a where c.created_by=a.id and a.id=1 order by sort_order,id;
 select c.id, c.name, a.street, a.postcode from contact c, address a where a.contact_id=c.id order by c.sort_order,id;
-select id,name,groups from contact where 'friends' = ANY(groups);
+select id,name,groups from contact where '4' = ANY(groups);
